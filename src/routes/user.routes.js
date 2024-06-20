33d0f94226493930059465a7a9873b4a318ff714
Gauhariqbal314 <gauhariqbal314@gmail.com>
2024-06-20 19:43:49 +0530

    add refresh end point

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { resisterUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, resisterUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
@@ -18,6 +19,13 @@ router.route("/register").post(
     ]),
     resisterUser
 )
+
+
+router.route("/login").post(loginUser)
+
+// secured route
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router
